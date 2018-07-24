ea3f07ca027977ce59abbb977588d1591dc7d807
VictorMan <victormanuelgomu@gmail.com>
2018-07-24 13:16:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/revisionequipamiento/Principal.kt
@@ -0,0 +1,15 @@
+package com.example.revisionequipamiento
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_principal.*
+
+class Principal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}
