39c1b80fbcc735c4b92ef9bba8d5458abbd6dbfe
VictorMan <victormanuelgomu@gmail.com>
2018-07-25 10:05:42 +0200

    SplashScreen funcionando

---
+++ app/src/main/java/com/example/revisionequipamiento/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.revisionequipamiento
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val background = object :Thread(){
+            override fun run() {
+                try {
+                    Thread.sleep(3000)
+                    val intent =Intent(baseContext,Principal::class.java)
+                    startActivity(intent)
+                }catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
